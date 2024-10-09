6466c0c5eaf9cecf53c8c3403656ee09f9e3dbdd
Алексей <ix.fenix@mail.ru>
2024-10-10 02:28:35 +0300

    fix: fixed camera shake & castle button

--- LudumDare56/Assets/_Source/GameAssembly/Player/HealthSoundsIndicator.cs
+++ LudumDare56/Assets/_Source/GameAssembly/Player/HealthSoundsIndicator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using Player.Data;
 using UnityEngine;
 using Utils;
@@ -9,6 +8,8 @@ namespace Player
 {
 	public class HealthSoundsIndicator : MonoBehaviour
 	{
+		[SerializeField] private float maxAmplitude = 2f;
+		[SerializeField] private float maxFrequency = 2f;
 		[SerializeField] private AudioSource source;
 		[SerializeField] private AudioClip heartSound;
 		[SerializeField] private float soundCheckInterval;
@@ -26,19 +27,33 @@ namespace Player
 
 		private void Start() => StartCoroutine(StartHealthSoundsCoroutine());
 
+		private void FixedUpdate()
+		{
+			const float offset = 0.5f;
+			if (_health.Health / _config.MaxHealth < offset)
+			{
+				var relativeValue = _config.MaxHealth - _health.Health / _config.MaxHealth - offset;
+				var scaledAmplitude = relativeValue * maxAmplitude * 2;
+				var scaledFrequency = relativeValue * maxAmplitude * 2;
+
+				cameraShaker.StartShake(gameObject,
+					Mathf.Clamp(scaledAmplitude, 0, maxAmplitude),
+					Mathf.Clamp(scaledFrequency, 0, maxFrequency));
+			}
+			else
+				cameraShaker.StopShake(gameObject);
+		}
+
 		// ReSharper disable once FunctionRecursiveOnAllPaths
 		private IEnumerator StartHealthSoundsCoroutine()
 		{
-			if(_health.IsDead)
+			if (_health.IsDead)
 				yield break;
-			
-			yield return new WaitForSeconds(Mathf.Clamp(soundCheckInterval * (_health.Health / _config.MaxHealth), 0.6f, soundCheckInterval));
 
-			if(_health.Health / _config.MaxHealth < 0.2f)
-				cameraShaker.StartShake(gameObject);
-			else
-				cameraShaker.StopShake(gameObject);
-			
+			yield return new WaitForSeconds(Mathf.Clamp(soundCheckInterval * (_health.Health / _config.MaxHealth),
+				maxAmplitude,
+				soundCheckInterval));
+
 			if (Mathf.Approximately(_health.Health, _config.MaxHealth))
 			{
 				StartCoroutine(StartHealthSoundsCoroutine());
--- LudumDare56/Assets/_Source/GameAssembly/Utils/CameraShaker.cs
+++ LudumDare56/Assets/_Source/GameAssembly/Utils/CameraShaker.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
@@ -7,34 +6,67 @@ namespace Utils
 {
 	public class CameraShaker : MonoBehaviour
 	{
-		private CinemachineBasicMultiChannelPerlin _vCam;
-		private readonly List<GameObject> _invokers = new();
+		[SerializeField] private float shakeAmplitude = 1.2f;
+		[SerializeField] private float shakeFrequency = 2.0f;
+		[SerializeField] private CinemachineVirtualCamera virtualCamera;
 
-		private void Awake()
+		private CinemachineBasicMultiChannelPerlin _virtualCameraNoise;
+		private readonly List<GameObject> _invokers = new();
+		private bool _isShaking;
+		private float _selectedAmplitude;
+		private float _selectedFrequency;
+
+		private void Start()
 		{
-			_vCam = GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
+			if (virtualCamera != null)
+				_virtualCameraNoise =
+					virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+			_selectedAmplitude = shakeAmplitude;
+			_selectedFrequency = shakeFrequency;
 		}
 
-		private void Start() => _vCam.enabled = false;
-
-		public void StartShake(GameObject invoker)
+		private void Update()
 		{
+			if (_isShaking)
+			{
+				_virtualCameraNoise.m_AmplitudeGain = _selectedAmplitude;
+				_virtualCameraNoise.m_FrequencyGain = _selectedFrequency;
+			}
+			else
+			{
+				_virtualCameraNoise.m_AmplitudeGain = 0f;
+				_virtualCameraNoise.m_FrequencyGain = 0f;
+			}
+		}
+
+		public void StartShake(GameObject invoker, float shakeAmpl = 0, float shakeFreq = 0)
+		{
+			_selectedAmplitude = shakeAmpl == 0 ? shakeAmplitude : shakeAmpl;
+			_selectedFrequency = shakeFreq == 0 ? shakeFrequency : shakeFreq;
+
+			if (virtualCamera == null || _virtualCameraNoise == null)
+				return;
+
 			if (_invokers.Contains(invoker))
 				return;
-			
+
 			_invokers.Add(invoker);
-			_vCam.enabled = true;
+			_isShaking = true;
 		}
 
 		public void StopShake(GameObject invoker)
 		{
+			if (virtualCamera == null || _virtualCameraNoise == null)
+				return;
+
 			if (!_invokers.Contains(invoker))
 				return;
 
 			_invokers.Remove(invoker);
 
 			if (_invokers.Count == 0)
-				_vCam.enabled = false;
+				_isShaking = false;
 		}
 	}
 }
